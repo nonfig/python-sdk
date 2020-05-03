bbc418b18eeb79d1d70df539eead807e29cc5004
Hamza Waqas <ArkeologeN@users.noreply.github.com>
2020-05-03 23:44:14 +0200

    feat: initial project structure

---
+++ test.py
@@ -0,0 +1,10 @@
+from api import Nonfig
+
+options = {
+  "app_id": "__ID__",
+  "app_secret": "__SECRET__",
+  "debug": True,
+  "cache_enable": True,
+  "cache_ttl": 60
+}
+nonfig = Nonfig(app_id="haha", app_secret="haha")
